b4fe42f0381ccbaaf782d6f00f107ba25e8ccfd1
Jeffry Babb <ahuimanu@gmail.com>
2022-08-20 11:09:16 -0500

    updated kml parser

--- parseKML.py
+++ parseKML.py
@@ -4,20 +4,35 @@ from lxml import etree
 
 # lxml guidance: https://www.python101.pythonlibrary.org/chapter31_lxml.html
 
+
 @click.command()
-@click.argument('infile')
-@click.argument('outfile')
+@click.argument("infile")
+@click.argument("outfile")
 def main(infile, outfile):
-    click.echo(f'checking {infile}')
+    click.echo(f"checking {infile}")
 
     with open(infile) as ifile:
         xml = ifile.read()
 
+    # develop root document node
+    # https://stackoverflow.com/questions/57833080/how-to-fix-unicode-strings-with-encoding-declaration-are-not-supported
+    root = etree.fromstring(bytes(xml, encoding="utf8"))
+
+    for appt in root.getchildren():
+        for elem in appt.getchildren():
+            if not elem.text:
+                text = "None"
+            else:
+                text = elem.text
+
+            click.echo(f"{elem.tag} => {text}")
+
     # KML file
-    infile = ''
+    infile = ""
 
     # P3D XML Placement File
-    outfile = ''
+    outfile = ""
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
